ebfd6ba3a53e2664f2daf700c7bef3fd52a01a4b
MrFan <92414382+MrFanCode@users.noreply.github.com>
2022-08-27 12:11:16 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, url_for, redirect
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, TextAreaField
+from morse import MorseCodeTranslator
+
+TRANSLATOR = MorseCodeTranslator()
+
+app = Flask(__name__)
+app.config['SECRET_KEY']= '627474ba186e2bfcde98adc23942c60e4e'
+
+class TransForm(FlaskForm):
+    morse = TextAreaField("Morse Code Translate")
+    submit_btn = SubmitField("Translate")
+
+@app.route('/')
+def home():
+    return render_template('home.html', title="MrFnMorseTrans")
+
+
+@app.route('/text-to-morse', methods=['GET', 'POST'])
+def text_to_morse():
+    form = TransForm()
+    if form.validate_on_submit():
+        data = form.data['morse']
+        if data == "":
+            return redirect(url_for('text_to_morse'))
+        elif data != "":
+            data = TRANSLATOR.translate_text(data)
+            return render_template('text-to-morse.html', title='Text to Morse', form=form, data=data) 
+    return render_template('text-to-morse.html', title='Text to Morse', form=form)
+
+
+@app.route('/morse-to-text', methods=['GET', 'POST'])
+def morse_to_text():
+    form = TransForm()
+    if form.validate_on_submit():
+        data = form.data['morse']
+        if data == "":
+            return redirect(url_for('morse_to_text'))
+        elif data != "":
+            data = TRANSLATOR.translate_morse(data)
+            return render_template('text-to-morse.html', title='Text to Morse', form=form, data=data)
+    return render_template('morse-to-text.html', title='Morse to Text', form=form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ usage-ex.py
@@ -0,0 +1,22 @@
+from morse import MorseCodeTranslator
+
+translator = MorseCodeTranslator()
+
+msg = "hello"
+
+morse = translator.translate_text(msg)
+
+print(morse)
+
+# translator = MorseCodeTranslator()
+
+# text = "This string has been translated to morse code and back again"
+
+# # Translate text to morse code
+# morse = translator.translate_text(text)
+
+# # Translate morse code to text
+# translated_text = translator.translate_morse(morse)
+
+# print(morse)
+# print(translated_text)
